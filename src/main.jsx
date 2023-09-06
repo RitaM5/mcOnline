ebb9aecb0c4fa53df90e99cfb8234305cc1fd271
RitaM5 <sporshia108514orva@gmail.com>
2023-09-06 21:02:34 +0600

    mcOnile project setup done

---
+++ src/main.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '/',
+        element: <Home />
+      },
+    ],
+  },
+])
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <>
+    <RouterProvider router={router} />
+  </>
+)
+
